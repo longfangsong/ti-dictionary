17ee80007d0241dc33585862e6f636ea0cef9211
longfangsong <longfangsong@icloud.com>
2021-01-17 00:36:22 +0800

    Cleanup

--- generate_json.ts
+++ generate_json.ts
@@ -12,17 +12,19 @@ const fs = require('fs');
 
 function generate(language: string) {
     const data = fs.readFileSync(`./${language}.md`, 'utf-8');
-    const rows: Array<Array<string>> = data.split('\n').slice(2).map(it => it.trim()).filter(it => it !== '')
-        .map(it => it.split('|').map(it => it.trim()).slice(1));
+    const rows: Array<Array<string>> = data.split('\n')
+        .slice(2)
+        .map((it: string) => it.trim()).filter((it: string) => it !== '')
+        .map((it: string) => it.split('|').map(it => it.trim()).slice(1));
     let result: Array<Item> = [];
     let currentName = "";
-    for(const row of rows) {
-        if(row[0] !== '') {
+    for (const row of rows) {
+        if (row[0] !== '') {
             currentName = row[0];
         }
         const aliases = row[1].split(",").map(it => it.trim()).filter(it => it !== '');
         const links = row[4].split(",")
-            .map(it => it.replace('[','').replace('(','').replace(')',''))
+            .map(it => it.replace('[', '').replace('(', '').replace(')', ''))
             .map(it => it.split(']').map(it => it.trim()).filter(it => it !== ""))
             .filter(it => it.length !== 0);
         result.push({name: currentName, alias: aliases, category: row[2], introduction: row[3], links: links as any});
